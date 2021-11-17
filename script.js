240419538b782d2f59d40085576b600b5c830086
Ehor <eevu2277@ukr.net>
2021-11-17 22:22:43 +0200

    new lesson

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+if (1) {
+    console.log('Ok!');
+
+} else {
+    console.log('Error');
+}
+
+
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Okay');
+// }
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+// : значит если / 
+
+const num = 50;
+
+switch (num) {
+ case 49: 
+ console.log('Неверно');
+ break;
+case 100: console.log('Unright');
+break;
+case 50: console.log('All right');
+break;
+default: console.log(' not that time');
+break;
+}
